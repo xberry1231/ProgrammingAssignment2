aaa224265b35ca2ded19f2daad3d085b9578c2ee
xberry1231 <xander.berry13@gmail.com>
2015-02-22 16:10:16 -0500

    First commit
    Please enter the commit message for your changes. Lines starting

--- cachematrix.R
+++ cachematrix.R
@@ -12,4 +12,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     inv <- x$getInverse()
+     if(!is.null(inv)){
+          message("getting cached data")
+          return(inv)
+     }
+     data <- x$get()
+     inv <- solve(data,...)
+     x$setInverse(inv)
+     inv
 }
